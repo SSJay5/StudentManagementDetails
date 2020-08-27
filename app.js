59fc5dc8c921ec92f199bb03996e6fdf2117eebe
SSJay5 <48097747+SSJay5@users.noreply.github.com>
2020-08-27 14:04:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const AppError = require('./starter/utils/appError');
 const userRouter = require('./starter/routes/userRoutes');
 const internshipRouter = require('./starter/routes/internshipRoutes');
 const projectRouter = require('./starter/routes/projectRoutes');
+const academicsRouter = require('./starter/routes/academicsRoutes');
 const globalErrorHandler = require('./starter/controller/errorController.js');
 
 app.use(helmet());
@@ -56,6 +57,7 @@ app.use(
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/internship', internshipRouter);
 app.use('/api/v1/project', projectRouter);
+app.use('/api/v1/academics', academicsRouter);
 app.use('*', (req, res, next) => {
   return next(
     new AppError(`Can't find ${req.originalUrl} on this server!!!`, 404)
