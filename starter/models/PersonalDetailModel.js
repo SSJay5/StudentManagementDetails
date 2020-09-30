39203ae4bb6c678fb2ca44633e9df48af3febed0
Siddhesh Dandavatimath <55954305+sidd7777@users.noreply.github.com>
2020-09-30 14:53:58 +0530

    Update PersonalDetailModel.js

--- starter/models/PersonalDetailModel.js
+++ starter/models/PersonalDetailModel.js
@@ -6,58 +6,58 @@ var detailsSchema =  new Schema({
 
     firstname : {
         type : String,
-        required  :  true
+        required  :  [true, "Please provide your first name"]
     },
 
     middlename: {
         type : String,
-        required : true 
+        required : [true, "Please provide your middle name"]
 
     },
 
     lastname : {
         type : String,
-        required :true   
+        required : [true, "Please provide your last name"] 
     },
 
     dateOfBirth : {
         type : Date,
-        required : true
+        required : [true, "Please provide your Date of Birth"]
     },
 
     collegeId : {
         type : String,
-        required : true,
+        required : [true, "Please provide your College ID"],
         unique : true  
     },
 
     contact : {
         type : Number,
-        required : true,
+        required : [true, "Please provide your contact number"],
         min : 10,
         max : 10
     },
 
     contactParent : {
         type : Number,
-        required : true,
+        required : [true, "Please provide your Parent's Contact Number"],
         min : 10,
         max : 10
     },
 
     address :{ 
         type :String,
-        required : true
+        required : [true, "Please provide your Address"]
     },
     
     addressPermanent : {
         type : String,
-        required : true
+        required : [true, "Please provide your Permanent Address"]
     },
 
     profilePhoto : {
         type : String,
-        required : true
+        required : [true, "Please provide your Profile Photo"]
     },
     
 })
