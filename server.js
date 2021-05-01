2436a930bc6269ff96c0a5011f0e73cf2c3555d8
Siddhesh Dandavatimath <55954305+sidd7777@users.noreply.github.com>
2021-05-01 12:51:41 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION Shutting Down......');
-  console.log(err);
-  process.exit(1);
+    console.log('UNCAUGHT EXCEPTION Shutting Down......');
+    console.log(err);
+    process.exit(1);
 });
 // eslint-disable-next-line import/no-absolute-path
 dotenv.config({ path: './config.env' });
@@ -13,32 +13,32 @@ const app = require('./app.js');
 //console.log(process.env.DATABASE);
 
 const DB = process.env.DATABASE.replace(
-  '<password>',
-  process.env.DATABASE_PASSWORD
+    '<password>',
+    process.env.DATABASE_PASSWORD
 );
 
 // const DB=process.env.DATABASE;
 // console.log(process.env);
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-   console.log('DB connection established');
-  });
-const port = 3000;
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log('DB connection established');
+    });
+const port = 5000;
 // eslint-disable-next-line import/no-absolute-path
 
 const server = app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log('UNNHANDELED REJECTION Shutting Down......');
-  console.log(err);
-  server.close(() => {
-    process.exit(1);
-  });
+    console.log('UNNHANDELED REJECTION Shutting Down......');
+    console.log(err);
+    server.close(() => {
+        process.exit(1);
+    });
 });
